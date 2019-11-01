9d47ed5bb9543139a56f2d467d2cbfc8a7669efd
patchcasey <patchcasey@gmail.com>
2019-11-01 09:49:24 -0400

    updating run by cli

--- pset_4/cli.py
+++ pset_4/cli.py
@@ -1,9 +1,11 @@
 from luigi import build
-from .tasks.data import ContentImage, DownloadImage
+from .tasks.data import ContentImage, DownloadImage, DownloadModel
+from .tasks.stylize import Stylize
 
 
 def main():
     build([
-        DownloadImage(
+        Stylize(
+            model='rain_princess.pth',
             image='luigi.jpeg'
         )], local_scheduler=True)
