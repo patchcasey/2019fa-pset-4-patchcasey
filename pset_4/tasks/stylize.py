aba4e78f52fb5d8794fa72e18249c6679d086fc2
patchcasey <patchcasey@gmail.com>
2019-11-06 22:24:20 -0500

    formatting and commenting

--- pset_4/tasks/stylize.py
+++ pset_4/tasks/stylize.py
@@ -4,11 +4,12 @@ import os
 from luigi import ExternalTask, Parameter, Task, LocalTarget, format
 from csci_utils import luigi as csci_luigi
 
-image_name = 'luigi.jpeg'
-model_name = 'udnie.pth'
+image_name = "luigi.jpeg"
+model_name = "udnie.pth"
+
 
 class Stylize(Task):
-    targetpath = os.path.join(os.getcwd(), 'data/')
+    targetpath = os.path.join(os.getcwd(), "data/")
     image_target = os.path.join(targetpath, image_name)
     model_target = os.path.join(targetpath, model_name)
 
@@ -16,28 +17,37 @@ class Stylize(Task):
     image = Parameter(image_target)
 
     def requires(self):
+        """
+        I have chosen to implement the S3Target tasks and downloading data tasks in my utils repo,
+        it both can be adjusted easily to serve a wider variety of purposes if further psets
+        require more Luigi work
+
+        :return: returns requirements for the stylize luigi task to run
+        """
         return {
-            'image': csci_luigi.DownloadImage(),
-            'model': csci_luigi.DownloadModel()
+            "image": csci_luigi.DownloadImage(),
+            "model": csci_luigi.DownloadModel(),
         }
 
     def output(self):
-        # return SuffixPreservingLocalTarget of the stylized image
-        targetpath = os.path.join(os.getcwd(), 'data/')
-        styled_image = os.path.join(targetpath, 'styled_image_Waluigi_udnie_test2.jpeg')
+        """
+        declares the output to check for should be the styled image
+
+        :return: SuffixPreservingLocalTarget of styled image
+        """
+        targetpath = os.path.join(os.getcwd(), "data/")
+        styled_image = os.path.join(targetpath, "styled_image_Waluigi_udnie_test2.jpeg")
         return csci_luigi.SuffixPreservingLocalTarget(styled_image, format=format.Nop)
 
     def run(self):
-        # For example
         inputs = self.input()
-        # with self.output().temporary_path() as temp_output_path:
+        # declaring args to handle stylize properly
         class args:
-            content_image = inputs['image'].path
-            model = inputs['model'].path
+            content_image = inputs["image"].path
+            model = inputs["model"].path
             output_image = self.output().path
             cuda = 0
             content_scale = 1
             export_onnx = False
 
         stylize(args)
-
--- test_pset.py
+++ test_pset.py
@@ -4,7 +4,21 @@
 """Tests for `pset_4` package."""
 
 from unittest import TestCase
+from luigi import build
+from pset_4.tasks.stylize import Stylize
+
 
 class Dummy_Test(TestCase):
     def test_dummy(self):
-        self.assertEqual(0,0)
+        self.assertEqual(0, 0)
+
+
+# class Luigi_Download_Tests(TestCase):
+# TODO - not implementing this test because it would download files during every travis test
+# def test_ContentImage(self):
+#     x = build([
+#         Stylize(
+#             model='udnie.pth',
+#             image='luigi.jpeg'
+#         )], local_scheduler=True)
+#     self.assertEqual(x, True)
